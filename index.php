c28261853d9777a55f7096a23e043c051af19fe5
Artur Agh <aghartur@gmail.com>
2024-06-04 14:46:56 +0200

    pagination done starting date

--- index.php
+++ index.php
@@ -3,43 +3,70 @@
 require __DIR__ . '/inc/functions.inc.php';
 require __DIR__ . '/inc/db-connect.inc.php';
 
-$stmt = $pdo->prepare("SELECT * FROM `entries`");
+$perPage = 2;
+$page = (int)($_GET['page'] ?? 1);
+if ($page < 1) $page = 1;
+
+$offset = ($page - 1) * $perPage;
+
+$stmtCount = $pdo->prepare('SELECT COUNT(*) AS `count` FROM `entries`');
+$stmtCount->execute();
+$count = $stmtCount->fetch(PDO::FETCH_ASSOC);
+
+$numPages = ceil($count['count'] / $perPage);
+
+
+$stmt = $pdo->prepare("SELECT * FROM `entries` ORDER BY `date` DESC, `id` DESC LIMIT :perPage OFFSET :offset");
+$stmt->bindValue('perPage', (int)$perPage, PDO::PARAM_INT);
+$stmt->bindValue('offset', (int)$offset, PDO::PARAM_INT);
 $stmt->execute();
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
+
+
+
+
 <?php require __DIR__ . '/views/header.php'; ?>
     <h1 class="main-heading">Entries</h1>
-            <?php foreach ($results as $result) : ?>
-                <div class="card">
-                    <div class="card__image-container">
-                        <img class="card__image" src="images/pexels-canva-studio-3153199.jpg" alt=""/>
-                    </div>
-                    <div class="card__desc-container">
-                        <div class="card__desc-time"><?php echo e($result['date']) ?></div>
-                        <h2 class="card__heading"><?php echo e($result['title']) ?></h2>
-                        <p class="card__paragraph">
-                            <?php echo nl2br(e($result['message'])) ?>
-                        </p>
-                    </div>
-                </div>
-            <?php endforeach ?>
+<?php foreach ($results as $result) : ?>
+    <div class="card">
+        <div class="card__image-container">
+            <img class="card__image" src="images/pexels-canva-studio-3153199.jpg" alt=""/>
+        </div>
+        <div class="card__desc-container">
+            <div class="card__desc-time"><?php echo e($result['date']) ?></div>
+            <h2 class="card__heading"><?php echo e($result['title']) ?></h2>
+            <p class="card__paragraph">
+                <?php echo nl2br(e($result['message'])) ?>
+            </p>
+        </div>
+    </div>
+<?php endforeach ?>
+<?php if ($numPages > 1) : ?>
 
-            <ul class="pagination">
-                <li class="pagination__li">
-                    <a class="pagination__link" href="#">⏴</a>
-                </li>
-                <li class="pagination__li">
-                    <a class="pagination__link pagination__link--active" href="#">1</a>
-                </li>
-                <li class="pagination__li">
-                    <a class="pagination__link" href="#">2</a>
-                </li>
-                <li class="pagination__li">
-                    <a class="pagination__link" href="#">3</a>
-                </li>
-                <li class="pagination__li">
-                    <a class="pagination__link" href="#">⏵</a>
-                </li>
-            </ul>
+    <ul class="pagination">
+        <?php if ($page > 1): ?>
+            <li class="pagination__li">
+                <a class="pagination__link"
+                   href="index.php?<?php echo http_build_query(['page' => $page - 1]); ?>">⏴</a>
+            </li>
+        <?php endif; ?>
+        <?php for ($i = 1; $i <= $numPages; $i++) : ?>
+            <li class="pagination__li">
+                <a class="pagination__link <?php echo $page === $i ? 'pagination__link--active' : '' ?>"
+                   href="index.php?<?php echo http_build_query(['page' => $i]); ?>">
+                    <?php echo e($i); ?>
+                </a>
+            </li>
+        <?php endfor; ?>
+        <?php if ($page < $numPages): ?>
+
+            <li class="pagination__li">
+                <a class="pagination__link"
+                   href="index.php?<?php echo http_build_query(['page' => $page + 1]); ?>">⏵</a>
+            </li>
+        <?php endif; ?>
+    </ul>
+<?php endif; ?>
 <?php require __DIR__ . '/views/footer.php'; ?>
